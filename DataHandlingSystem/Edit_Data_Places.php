8f5786d897ef56c6a2e44cdb25a0aa959f0d5f8c
chkounou <chkounou@gmail.com>
2015-03-18 08:58:36 +0200

    Minor edits
    Minor edits in code and addition of weekly minutes

--- DataHandlingSystem/Edit_Data_Places.php
+++ DataHandlingSystem/Edit_Data_Places.php
@@ -1,7 +1,7 @@
 <?php
 	include('Connect_DB.php');
 	
-	$query = "UPDATE PLACES SET PNAME = '$_POST[pname]', PDESCRIPTION = '$_POST[pdescription]', PTYPE = '$_POST[ptype]'  WHERE PID = '$_POST[pid]'";
+	$query = "UPDATE PLACE SET NAME = '$_POST[pname]', DESCRIPTION = '$_POST[pdescription]', TYPE = '$_POST[ptype]'  WHERE PID = '$_POST[pid]'";
 	mysql_query($query);
 	header("location: View_Places.php");
 ?>
